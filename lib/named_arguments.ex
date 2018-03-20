dfbc40fc8d8518854e50fa1752e973ed9c0b6a11
techgaun <coolsamar207@gmail.com>
2018-03-19 19:34:39 -0500

    generate custom def/2 & defp/2

--- lib/named_arguments.ex
+++ lib/named_arguments.ex
@@ -1,18 +1,24 @@
 defmodule NamedArguments do
   @moduledoc """
-  Documentation for NamedArguments.
+  NamedArguments allows you to have python-like named arguments with defaults.
   """
 
-  @doc """
-  Hello world.
+  @doc false
+  defmacro __using__(_opts) do
+    quote do
+      import Kernel, except: [def: 2, defp: 2]
+      import unquote(__MODULE__)
+    end
+  end
 
-  ## Examples
-
-      iex> NamedArguments.hello
-      :world
-
-  """
-  def hello do
-    :world
+  for fun_name <- ~w(def defp)a do
+    defmacro unquote(fun_name)(defn = {_, _, _}, do: body) do
+      fun = unquote(fun_name)
+      quote do
+        Kernel.unquote(fun)(unquote(defn)) do
+          unquote(body)
+        end
+      end
+    end
   end
 end
