8efab96880df79e7975ee2c5236e3f3531a8cae5
techgaun <coolsamar207@gmail.com>
2018-03-19 23:26:47 -0500

    update min version of elixir

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule NamedArguments.MixProject do
     [
       app: :named_arguments,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
